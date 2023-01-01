0cafa19bbf5e5dfd13a67a6da848589df081d14b
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2022-12-31 23:25:53 -0500

    adding photo of the day, still needs page design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
+  const [image, setImage] =useState('')
+  useEffect(()=>{
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DDTp3OITNbyP2TF7QJetNvjKNmyywdychw6LfiIa')
+    .then(res=>{
+      console.log(res.data.url);
+      setImage(res.data.url)
+    })
+    .catch(err=> console.error(err))
+  },[])
+
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        
       </p>
+      <img style={{width:'40%'}} src={image} alt="space"/>
     </div>
   );
 }
